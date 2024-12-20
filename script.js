7f3a4b5704da4470e8cdbcaf931196cc9c0b7ee8
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2024-12-20 17:03:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-function volume_sphere() {
+function volume_sphere(e) {
     //Write your code here
-  
+	e.preventDefault()
+ const r_input = document.querySelector("#radius")
+ const v_input = document.querySelector("#volume")
+	let radius = r_input.value;
+	let vol = 4/3*(3.14*radius**3)
+	v_input.value = vol
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
